84b7332e1a79c54c49875dda6236d900c9838ed0
Jacob Varns <jacob.varns@gmail.com>
2020-12-05 15:48:02 -0800

    Day 5

---
+++ Day5Part1.js
@@ -0,0 +1,27 @@
+let vals = document.querySelector("pre").innerText.split("\n");
+let tickets = [];
+
+for (let i = 0; i < vals.length - 1; i++) {
+  let rowMin = 0;
+  let rowMax = 127;
+  let leftMin = 0;
+  let leftMax = 7;
+  let dirs = vals[i].split("");
+  for (let j = 0; j < dirs.length; j++) {
+    if (dirs[j] == "F") {
+      rowMax = rowMax - (rowMax - rowMin + 1) / 2;
+    }
+    if (dirs[j] == "B") {
+      rowMin = rowMin + (rowMax - rowMin + 1) / 2;
+    }
+    if (dirs[j] == "L") {
+      leftMax = leftMax - (leftMax - leftMin + 1) / 2;
+    }
+    if (dirs[j] == "R") {
+      leftMin = leftMin + (leftMax - leftMin + 1) / 2;
+    }
+  }
+  tickets.push(rowMin * 8 + leftMin);
+}
+tickets = tickets.sort((a, b) => a - b);
+console.log(tickets[tickets.length - 1]);
